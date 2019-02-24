4e2af998514abf3a91b8d3b956652beb8437da66
silksil <silkreulen@gmail.com>
2019-02-24 20:12:51 +0100

    Updated icon packages

---
+++ projects/web/components/Wrapper/Wrapper.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import Header from "./Header";
+import Meta from "./Meta";
+import Link from "next/link";
+import { LocalStateButton } from "./testLocalState";
+import { Close } from "@benjaminf/icons";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'Arial';
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+  button {  font-family: 'Arial'; }
+`;
+
+class Wrapper extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+
+          <Link href="/">
+            <a> Boilerplate </a>
+            <Close />
+          </Link>
+          <LocalStateButton />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default Wrapper;
