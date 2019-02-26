4eb9efab796b66abcd90938f27fea862e9b1b2dd
silksil <silkreulen@gmail.com>
2019-02-26 09:41:50 +0100

    Installed babel and set-up config

---
+++ packages/breakpoints/dist/components/Breakpoints.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.breakpoints = exports.sizes = void 0;
+var sizes = {
+  xs: 480,
+  md: 768,
+  lg: 1024,
+  xl: 1440
+};
+exports.sizes = sizes;
+var breakpoints = {
+  xs: "@media (min-width: ".concat(sizes.xs, "px)"),
+  md: "@media (min-width: ".concat(sizes.md, "px)"),
+  lg: "@media (min-width: ".concat(sizes.lg, "px)"),
+  xl: "@media (min-width: ".concat(sizes.xl, "px)")
+};
+exports.breakpoints = breakpoints;
---
+++ packages/breakpoints/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "breakpoints", {
+  enumerable: true,
+  get: function get() {
+    return _Breakpoints.breakpoints;
+  }
+});
+Object.defineProperty(exports, "sizes", {
+  enumerable: true,
+  get: function get() {
+    return _Breakpoints.sizes;
+  }
+});
+
+var _Breakpoints = require("./components/Breakpoints");
